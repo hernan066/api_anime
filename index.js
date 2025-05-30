cf1c5fbaabdaa4132583f1daed48534ba61aa6ce
Hernan <hernan0066@gmail.com>
2025-05-29 23:51:57 -0300

    Refactor index.js by removing commented sections for cleaner code

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-// index.js
-
-// Importar las dependencias
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./src/database/config');
 
-// Middleware para parsear JSON
 app.use(express.json());
 
 app.use(cors());
-
-// ------------connect to db ------------
 
 const connection = async () => {
 	await dbConnection();
 };
 connection();
 
-// -------------routes-----------
 app.use('/api/anime', require('./src/routes/animeRoutes'));
 app.use('/api/ninja', require('./src/routes/ninjaRoutes'));
 app.use('/api/user', require('./src/routes/userRoutes'));
@@ -31,12 +24,10 @@ app.get('/video-proxy', async (req, res) => {
 	res.send(html);
 });
 
-// Ruta para manejar errores 404
 app.use((req, res) => {
 	res.status(404).send('Página no encontrada');
 });
 
-// Iniciar el servidor
 app.listen(process.env.PORT || 3000, () => {
 	console.log(
 		`Servidor escuchando en http://localhost:${process.env.PORT || 3000}`
